ae9e1964b786a66194567c3208234633ed09f8d4
novelkhan <novel4004@gmail.com>
2025-09-20 01:02:23 +0600

    User & RefreshToken entities are created with the support of IdentityUser

---
+++ InventoryWebAPI/Domain/Entities/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryWebAPI.Domain.Entities
+{
+    public class RefreshToken
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Token { get; set; }
+        public DateTime DateCreatedUtc { get; set; } = DateTime.UtcNow;
+        public DateTime DateExpiresUtc { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= DateExpiresUtc;
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
--- InventoryWebAPI/Domain/Entities/User.cs
+++ InventoryWebAPI/Domain/Entities/User.cs
@@ -1,6 +1,11 @@
-﻿namespace InventoryWebAPI.Domain.Entities
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace InventoryWebAPI.Domain.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
+        //The required properties by default come with IdentityUser
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
     }
 }
