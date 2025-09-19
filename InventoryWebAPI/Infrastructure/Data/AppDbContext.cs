2b2b5c26db15ad068684c942eee7a6a75bcfe8d7
novelkhan <novel4004@gmail.com>
2025-09-20 01:42:53 +0600

    First migration is applied using 'add-migration AddingUserToDatabase -o Infrastructure/Data/Migrations'

---
+++ InventoryWebAPI/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using InventoryWebAPI.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryWebAPI.Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //add-migration AddingUserToDatabase -o Infrastructure/Data/Migrations
+        }
+
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
