654983104b9fa7f7ca3217943897b1494bde13d0
novelkhan <novel4004@gmail.com>
2025-09-20 16:48:50 +0600

    Product & Caregory entities are added

---
+++ InventoryWebAPI/Domain/Entities/Inventory/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryWebAPI.Domain.Entities.Inventory
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ InventoryWebAPI/Domain/Entities/Inventory/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryWebAPI.Domain.Entities.Inventory
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        public string? ImageBase64 { get; set; }
+        [Required]
+        public Guid CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
