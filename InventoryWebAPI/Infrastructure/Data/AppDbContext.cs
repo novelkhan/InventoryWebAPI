b3d72d213359e7795212a2674ceed307918c84b4
novelkhan <novel4004@gmail.com>
2025-09-20 17:00:27 +0600

    add-migration AddingProductToDatabase -o Infrastructure/Data/Migrations

--- InventoryWebAPI/Infrastructure/Data/AppDbContext.cs
+++ InventoryWebAPI/Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using InventoryWebAPI.Domain.Entities;
+using InventoryWebAPI.Domain.Entities.Inventory;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,9 +10,12 @@ namespace InventoryWebAPI.Infrastructure.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             //add-migration AddingUserToDatabase -o Infrastructure/Data/Migrations
+            //add-migration AddingProductToDatabase -o Infrastructure/Data/Migrations
         }
 
 
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
