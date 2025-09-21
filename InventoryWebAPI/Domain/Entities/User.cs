c0d98009dcc9598c1a47ae55a065b0a8c9809c06
novelkhan <novel4004@gmail.com>
2025-09-21 06:21:02 +0600

    Initial test is successful, Codes are working, Final codes will be deployed to Render.com with the help of docker container

--- InventoryWebAPI/Domain/Entities/User.cs
+++ InventoryWebAPI/Domain/Entities/User.cs
@@ -4,7 +4,7 @@ namespace InventoryWebAPI.Domain.Entities
 {
     public class User : IdentityUser
     {
-        //The required properties by default come with IdentityUser
+        ///The required properties by default come with IdentityUser
         public DateTime DateCreated { get; set; } = DateTime.UtcNow;
         public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
     }
